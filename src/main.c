982a9a83f73a182172ca302ac7386b1dd6800785
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-12-24 15:27:18 -0800

    Add virtual serial port (CDC-ACM) callbacks and toggle LED on input

--- src/main.c
+++ src/main.c
@@ -108,27 +108,30 @@ static const struct {
 };
 
 /* Serial ACM interface */
+#define CDCACM_PACKET_SIZE 	64
+#define CDCACM_UART_ENDPOINT	0x03
+#define CDCACM_INTR_ENDPOINT	0x84
+
 static const struct usb_endpoint_descriptor uart_comm_endp[] = {{
 	.bLength = USB_DT_ENDPOINT_SIZE,
 	.bDescriptorType = USB_DT_ENDPOINT,
-	.bEndpointAddress = 0x84,
+	.bEndpointAddress = CDCACM_INTR_ENDPOINT,
 	.bmAttributes = USB_ENDPOINT_ATTR_INTERRUPT,
 	.wMaxPacketSize = 16,
 	.bInterval = 255,
 }};
 
-#define CDCACM_PACKET_SIZE 	64
 static const struct usb_endpoint_descriptor uart_data_endp[] = {{
 	.bLength = USB_DT_ENDPOINT_SIZE,
 	.bDescriptorType = USB_DT_ENDPOINT,
-	.bEndpointAddress = 0x03,
+	.bEndpointAddress = CDCACM_UART_ENDPOINT,
 	.bmAttributes = USB_ENDPOINT_ATTR_BULK,
 	.wMaxPacketSize = CDCACM_PACKET_SIZE,
 	.bInterval = 1,
 }, {
 	.bLength = USB_DT_ENDPOINT_SIZE,
 	.bDescriptorType = USB_DT_ENDPOINT,
-	.bEndpointAddress = 0x83,
+	.bEndpointAddress = 0x80 | CDCACM_UART_ENDPOINT,
 	.bmAttributes = USB_ENDPOINT_ATTR_BULK,
 	.wMaxPacketSize = CDCACM_PACKET_SIZE,
 	.bInterval = 1,
@@ -311,10 +314,100 @@ static void hid_set_config(usbd_device *dev, uint16_t wValue)
 	systick_interrupt_enable();
 	systick_counter_enable();
 }
+static void cdcacm_set_modem_state(usbd_device *dev, int iface, bool dsr, bool dcd)
+{
+	char buf[10];
+	struct usb_cdc_notification *notif = (void*)buf;
+	/* We echo signals back to host as notification */
+	notif->bmRequestType = 0xA1;
+	notif->bNotification = USB_CDC_NOTIFY_SERIAL_STATE;
+	notif->wValue = 0;
+	notif->wIndex = iface;
+	notif->wLength = 2;
+	buf[8] = (dsr ? 2 : 0) | (dcd ? 1 : 0);
+	buf[9] = 0;
+	usbd_ep_write_packet(dev, 0x82 + iface, buf, 10);
+}
+
+static int cdcacm_control_request(usbd_device *dev,
+		struct usb_setup_data *req, uint8_t **buf, uint16_t *len,
+		void (**complete)(usbd_device *dev, struct usb_setup_data *req))
+{
+	(void)dev;
+	(void)complete;
+	(void)buf;
+	(void)len;
+
+	switch(req->bRequest) {
+	case USB_CDC_REQ_SET_CONTROL_LINE_STATE:
+		cdcacm_set_modem_state(dev, req->wIndex, true, true);
+		return 1;
+	case USB_CDC_REQ_SET_LINE_CODING:
+		if(*len < sizeof(struct usb_cdc_line_coding))
+			return 0;
+
+		return 1;
+	}
+	return 0;
+}
+
+static void usbuart_usb_out_cb(usbd_device *dev, uint8_t ep)
+{
+	(void)ep;
+
+	char buf[CDCACM_PACKET_SIZE];
+	int len = usbd_ep_read_packet(dev, CDCACM_UART_ENDPOINT,
+					buf, CDCACM_PACKET_SIZE);
+
+
+	for(int i = 0; i < len; i++)
+		gpio_toggle(GPIOC, GPIO13);
+}
+
+void usbuart_usb_in_cb(usbd_device *dev, uint8_t ep)
+{
+	(void) dev;
+	(void) ep;
+}
+
+static void cdcacm_set_config(usbd_device *dev, uint16_t wValue)
+{
+	(void) wValue;
+
+	/* Serial interface */
+	usbd_ep_setup(dev, CDCACM_UART_ENDPOINT, USB_ENDPOINT_ATTR_BULK,
+	              CDCACM_PACKET_SIZE, usbuart_usb_out_cb);
+	usbd_ep_setup(dev, 0x80 | CDCACM_UART_ENDPOINT, USB_ENDPOINT_ATTR_BULK,
+	              CDCACM_PACKET_SIZE, usbuart_usb_in_cb);
+	usbd_ep_setup(dev, CDCACM_INTR_ENDPOINT, USB_ENDPOINT_ATTR_INTERRUPT, 16, NULL);
+
+	usbd_register_control_callback(dev,
+			USB_REQ_TYPE_CLASS | USB_REQ_TYPE_INTERFACE,
+			USB_REQ_TYPE_TYPE | USB_REQ_TYPE_RECIPIENT,
+			cdcacm_control_request);
+
+	/* Notify the host that DCD is asserted.
+	 * Allows the use of /dev/tty* devices on *BSD/MacOS
+	 */
+	cdcacm_set_modem_state(dev, 2, true, true);
+}
+
+static void usb_set_config(usbd_device *dev, uint16_t wValue)
+{
+	hid_set_config(dev, wValue);
+	cdcacm_set_config(dev, wValue);
+}
+
 
 int main(void)
 {
 	rcc_clock_setup_in_hsi_out_48mhz();
+
+	rcc_periph_clock_enable(RCC_GPIOC);
+	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ,
+		GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
+	gpio_set(GPIOC, GPIO13);
+
 
 	rcc_periph_clock_enable(RCC_GPIOA);
 	/*
@@ -336,7 +429,7 @@ int main(void)
 	usbd_dev = usbd_init(&st_usbfs_v1_usb_driver, &dev_descr, &config, usb_strings,
 		sizeof(usb_strings)/sizeof(char *),
 		usbd_control_buffer, sizeof(usbd_control_buffer));
-	usbd_register_set_config_callback(usbd_dev, hid_set_config);
+	usbd_register_set_config_callback(usbd_dev, usb_set_config);
 
 	while (1)
 		usbd_poll(usbd_dev);
