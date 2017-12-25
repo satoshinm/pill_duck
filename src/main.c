4f355634ce241bf22731cf8dc7bcd0a05d1d95d8
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-12-24 17:44:20 -0800

    Add USB keyboard as composite device with USB mouse

--- src/main.c
+++ src/main.c
@@ -50,8 +50,42 @@ const struct usb_device_descriptor dev_descr = {
 // Generated using http://eleccelerator.com/usbdescreqparser/
 static const uint8_t hid_report_descriptor[] = {
 	0x05, 0x01,        // Usage Page (Generic Desktop Ctrls)
+	0x09, 0x06,        // Usage (Keyboard)
+	0xA1, 0x01,        // Collection (Application)
+	0x85, 0x01,        //   Report ID (1)
+	0x05, 0x07,        //   Usage Page (Kbrd/Keypad)
+	0x19, 0xE0,        //   Usage Minimum (0xE0)
+	0x29, 0xE7,        //   Usage Maximum (0xE7)
+	0x15, 0x00,        //   Logical Minimum (0)
+	0x25, 0x01,        //   Logical Maximum (1)
+	0x75, 0x01,        //   Report Size (1)
+	0x95, 0x08,        //   Report Count (8)
+	0x81, 0x02,        //   Input (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+	0x95, 0x01,        //   Report Count (1)
+	0x75, 0x08,        //   Report Size (8)
+	0x81, 0x03,        //   Input (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
+	0x95, 0x05,        //   Report Count (5)
+	0x75, 0x01,        //   Report Size (1)
+	0x05, 0x08,        //   Usage Page (LEDs)
+	0x19, 0x01,        //   Usage Minimum (Num Lock)
+	0x29, 0x05,        //   Usage Maximum (Kana)
+	0x91, 0x02,        //   Output (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+	0x95, 0x01,        //   Report Count (1)
+	0x75, 0x03,        //   Report Size (3)
+	0x91, 0x03,        //   Output (Const,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
+	0x95, 0x06,        //   Report Count (6)
+	0x75, 0x08,        //   Report Size (8)
+	0x15, 0x00,        //   Logical Minimum (0)
+	0x25, 0x65,        //   Logical Maximum (101)
+	0x05, 0x07,        //   Usage Page (Kbrd/Keypad)
+	0x19, 0x00,        //   Usage Minimum (0x00)
+	0x29, 0x65,        //   Usage Maximum (0x65)
+	0x81, 0x00,        //   Input (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
+	0xC0,              // End Collection
+	0x05, 0x01,        // Usage Page (Generic Desktop Ctrls)
 	0x09, 0x02,        // Usage (Mouse)
 	0xA1, 0x01,        // Collection (Application)
+	0x85, 0x02,        //   Report ID (2)
 	0x09, 0x01,        //   Usage (Pointer)
 	0xA1, 0x00,        //   Collection (Physical)
 	0x05, 0x09,        //     Usage Page (Button)
@@ -87,7 +121,7 @@ static const uint8_t hid_report_descriptor[] = {
 	0x95, 0x01,        //   Report Count (1)
 	0xB1, 0x01,        //   Feature (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
 	0xC0,              // End Collection
-};
+	};
 
 static const struct {
 	struct usb_hid_descriptor hid_descriptor;
@@ -358,9 +392,11 @@ static bool jiggler = true;
 void sys_tick_handler(void)
 {
 	static int x = 0;
-	uint8_t buf[4] = {0, 0, 0, 0};
+	uint8_t buf[5] = {0, 0, 0, 0, 0};
 
-	buf[1] = dir;
+	buf[0] = 2; // mouse
+
+	buf[2] = dir;
 	if (jiggler) {
 		x += dir;
 		if (x > 30)
